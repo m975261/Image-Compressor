5d0ab7e533b437c216494715bb2c121d0b713788
m975261 <63980628+m975261@users.noreply.github.com>
2025-12-26 20:06:26 +0400

    Add client/src/App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,12 +6,30 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 import TempDriveShare from "@/pages/temp-drive-share";
+import { HomeLogin } from "@/components/home-login";
+
+function AuthenticatedRouter() {
+  return (
+    <HomeLogin>
+      <Home />
+    </HomeLogin>
+  );
+}
 
 function Router() {
+  const [location] = useLocation();
+  
+  if (location.startsWith("/temp-drive/share/")) {
+    return (
+      <Switch>
+        <Route path="/temp-drive/share/:token" component={TempDriveShare} />
+      </Switch>
+    );
+  }
+
   return (
     <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/temp-drive/share/:token" component={TempDriveShare} />
+      <Route path="/" component={AuthenticatedRouter} />
       <Route component={NotFound} />
     </Switch>
   );
